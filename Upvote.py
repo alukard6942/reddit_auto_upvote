bfcff7aea287f3b94ac9e6f6e988c9c8779d3d6e
aetherofchao <aetherofchao@gmail.com>
2019-06-19 15:59:39 +0000

    kdjflak

--- Upvote.py
+++ Upvote.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 import praw
 import random
+import time 
 
 reddit = praw.Reddit('bot1', user_agent="bot1")
 
-subreddit = reddit.subreddit("nsfw")
-repost = reddit.subreddit("ass")
+subreddit = reddit.subreddit("blowjob")
+repost = reddit.subreddit("blowjobs")
 
-i =0
+i = 0
 #subreddit.submit("submission test", selftext = "hello world")
-for submission in subreddit.new(limit=500):
-    if (random.randint(0,100) == 0):
-    	repost.submit(submission.title, url=submission.url)
-    	print (" -->")
-    else: 
-    	print ".",
-    
+for submission in subreddit.new(limit=100):
+    if (i > 13):
+        repost.submit(submission.title, url=submission.url)
+        print ("I JUST POSTED " + str(i) + ". POST: " + submission.title)
+        time.sleep(60*10 + random.randint(10,60))
+    else:
+	    print ".",
     i += 1
 
 
