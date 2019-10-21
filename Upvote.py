d6399635890b9468144932d30eba9a91a420105d
aetherofchao <aetherofchao@gmail.com>
2019-10-21 16:06:05 +0200

    no idea budy you better figure it out on your own

--- Upvote.py
+++ Upvote.py
@@ -2,22 +2,61 @@
 import praw
 import random
 import time 
+import sys
+from urllib.request import urlopen
+from configparser import ConfigParser
 
-reddit = praw.Reddit('bot2', user_agent="bot2")
+def main():
+	
 
-subreddit = reddit.subreddit("blowjob")
-repost = reddit.subreddit("blowjobs")
+	is_shadow_baned("bot1")
 
-i = 0
-#subreddit.submit("submission test", selftext = "hello world")
-for submission in subreddit.new(limit=100):
-    if (i > 13):
-        repost.submit(submission.title, url=submission.url)
-        print ("I JUST POSTED " + str(i) + ". POST: " + submission.title)
-        time.sleep(60*10 + random.randint(10,60))
-    else:
-	    print ".",
-    i += 1
+	countdown(6*60)
+
+	# reddit = praw.Reddit('bot2', user_agent="bot2")
+
+	# subreddit = reddit.subreddit("JapaneseHotties")
+	# repost = reddit.subreddit("asianhotties")
+
+	# i = 0
+	# #subreddit.submit("submission test", selftext = "hello world")
+	# for submission in subreddit.new(limit=100):
+	#     if (i > 13):
+	#     	countdown(60*10 + random.randint(100,500))
+	#         repost.submit(submission.title, url=submission.url)
+	#         print ("I JUST POSTED " + str(i) + ". POST: " + submission.title)
+	#     else:
+	# 	    pprint( ".",end='')
+	#     i += 1
+
+def countdown(t):
+	sys.stdout.write("[")
+	sys.stdout.flush()
+	while (t != 0):
+		mins, secs = divmod(t, 60)
+		timeformat = '{:02d}:{:02d}'.format(mins, secs)
+		if (t %10 == 0): 
+			sys.stdout.write('.')
+			sys.stdout.flush()
+		time.sleep(1)
+		t -= 1
+	sys.stdout.write("]")
+	sys.stdout.flush()
+
+
+def is_shadow_baned(usr):
+
+	users = ConfigParser()
+	users.read('praw.ini')
+
+	url = 'https://www.reddit.com/user/'+users.get(usr, 'username')+'/about,json'
+	print( url)
+
+	response = urlopen("https://www.reddit.com")
+	countdown(10)
+	print (response.read())
 
 
 
+if (__name__ == '__main__'):
+	main()
---
+++ resourse.md
@@ -0,0 +1,16 @@
+# Resourses about reddit and its API 
+
+## API
+https://www.pythonforbeginners.com/python-on-the-web/how-to-use-reddit-api-in-python/
+https://www.pythonforengineers.com/build-a-reddit-bot-part-1/
+https://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html#praw-ini-files
+https://github.com/shantnu/RedditBot/blob/master/Part1/bot_read.py
+https://pypi.org/project/praw/
+
+## shadow bans
+https://www.quora.com/How-do-the-various-Reddit-bans-work
+http://findnerd.com/list/view/How-to-Avoid-shadowban-or-Account-suspension-on-Reddit/18427/
+https://www.reddit.com/r/ShadowBan/comments/1vyaa2/a_guide_to_getting_unshadowbanned_sticky_maybe/
+https://nullprogram.com/am-i-shadowbanned/#sendnudes9436
+
+## autoarization
